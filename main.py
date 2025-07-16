70bb3b00f8d8658ab5e40013459993ec0cec1195
XodTech <82662318+XodTech@users.noreply.github.com>
2025-07-16 13:11:08 +0200

    Fix: Corrected getting module name

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import importlib
 import os
 from fastapi import FastAPI
 
-ACTIVE_MODULES_DIR = "modules/active/"
+ACTIVE_MODULES_DIR = "modules/active"
 
 app = FastAPI()
 
 for filename in os.listdir(ACTIVE_MODULES_DIR):
     if filename.endswith(".py"): #NOTE: Consider excluding __init__.py
-        MODULE_NAME = f"{ ACTIVE_MODULES_DIR }.{filename[:-3]}"
+        MODULE_NAME = f"{ ACTIVE_MODULES_DIR.replace('/',".") }.{filename[:-3]}"
         module = importlib.import_module(MODULE_NAME)
         app.include_router(module.router)
