9b2fb0f582cff230bba7cdbce378ed2dd8aca8d8
XodTech <82662318+XodTech@users.noreply.github.com>
2025-07-16 13:02:55 +0200

    Feature: Implemented module system

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello World!")
+#pylint: disable=missing-docstring
+#pylint: disable=import-error
 
+import importlib
+import os
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    main()
+ACTIVE_MODULES_DIR = "modules/active/"
+
+app = FastAPI()
+
+for filename in os.listdir(ACTIVE_MODULES_DIR):
+    if filename.endswith(".py"): #NOTE: Consider excluding __init__.py
+        MODULE_NAME = f"{ ACTIVE_MODULES_DIR }.{filename[:-3]}"
+        module = importlib.import_module(MODULE_NAME)
+        app.include_router(module.router)
