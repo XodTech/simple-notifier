fa91ead7070b93ef2724f36051749eb1f356e4c7
XodTech <82662318+XodTech@users.noreply.github.com>
2025-07-17 15:04:02 +0200

    Chore: Provided telegram_handler output type for conciseness

--- modules/avaliable/telegram.py
+++ modules/avaliable/telegram.py
@@ -8,7 +8,7 @@ from aiogram import Bot
 
 # Configuration for aiogram
 BOT_TOKEN = ""  # Telegram bot token (required for authentication)
-DESTINATION_ID =0 # Replace it with your actual chat ID, group ID or channel ID
+DESTINATION_ID = 0 # Replace it with your actual chat ID, group ID or channel ID
 
 # Message related configuration
 DEFAULT_MESSAGE = "Ping"  # Default message to send if no message is provided
@@ -18,7 +18,7 @@ MESSAGE_FORMAT = "[msg]" # [msg] will be replaced with an actual message
 router = APIRouter()
 
 @router.post("/telegram")
-async def handle_telegram(msg:str = DEFAULT_MESSAGE):
+async def handle_telegram(msg:str = DEFAULT_MESSAGE) -> dict:
     try:
         bot = Bot(token=BOT_TOKEN)
         await bot.send_message(DESTINATION_ID,MESSAGE_FORMAT.replace("[msg]",msg))
