bf11db6cca2a1d5ac6ae34d2f5efbc10222c941d
XodTech <82662318+XodTech@users.noreply.github.com>
2025-07-21 12:02:28 +0200

    Chore: Renamed function for sending messages

--- modules/avaliable/discord_bot.py
+++ modules/avaliable/discord_bot.py
@@ -12,13 +12,13 @@ CHANNEL_ID = "" # Replace with your channel id
 router = APIRouter()
 
 
-def send_message(msg: str) -> dict:
+def send_bot_message(msg: str) -> dict:
     url = f"https://discord.com/api/v10/channels/{CHANNEL_ID}/messages"
     headers = {
         "Authorization": f"Bot {BOT_TOKEN}",
         "Content-Type": "application/json",
     }
-    data = {"content":msg}
+    data = {"content": msg}
     response = requests.post(url,headers=headers,json=data)
     return {
             "status_code": response.status_code,
@@ -27,7 +27,7 @@ def send_message(msg: str) -> dict:
 
 @router.post("/discord/bot")
 def handle_discord_bot(msg:str) -> dict:
-    response = send_message(msg)
+    response = send_bot_message(msg)
     if (status_code := response["status_code"]) == 200: #pylint: disable=no-else-return
         return {
             "status": 200,
